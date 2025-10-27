7078f82de8f01fcc1316b338cbf7b897787ae705
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-27 10:57:59 +0630

    arrange src folder

--- backend/src/server.py
+++ backend/src/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world123"
+    return "hello world"
 
 if __name__ in "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
